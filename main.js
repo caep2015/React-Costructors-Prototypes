3a0f9c6d2d2e2651923e0cbbb5b3ca63bad1a492
Carlota Pearl <carlota.pearl@gmail.com>
2017-08-21 17:22:05 -0400

    updated to remove super pet prototypes eventhough the tests passed- all tests pass

--- main.js
+++ main.js
@@ -7,48 +7,42 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog (name, status, color, hungry, owner) {
-  this.name = name,
-  this.status = status,
-  this.color = color,
-  this.hungry = hungry,
-  this.owner = owner;
-}
+function Dog (status, color, hungry, owner) {
+	  this.status = status
+	  this.color = color
+	  this.hungry = hungry
+	  this.owner = owner
+	}
+	// Instances of Dog
+	// Needed: sadie, moonshine, atticus
 
-Dog.prototype.pet = function(){
-  this.status = 'happy';
-};
-Dog.prototype.feed = function() {
-  this.hungry = false;
-};
 
-// Instances of Dog
-// Needed: sadie, moonshine, atticus
+	const sadie = new Dog('normal', 'black', false)
+	const moonshine = new Dog('normal', 'brown', true)
+	const atticus = new Dog('normal', 'grey', true)
 
-const sadie = new Dog('sadie', 'normal', 'black', false);
-const moonshine = new Dog('moonshine', 'normal', 'black', true);
-const atticus = new Dog('atticus');
+
 //     __
 //    / /_  __  ______ ___  ____ _____  _____
 //   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
 //  / / / / /_/ / / / / / / /_/ / / / (__  )
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
-//Human Constructor & Prototype
-function Human (name, cool) {
-  this.name = name;
-  this.cool = cool;
-}
-Human.prototype.pet = function(dog) {
-  dog.pet();
-};
-Human.prototype.feed = function(dog) {
-  dog.feed();
-};
+// Human Constructor & Prototype
+	function Human (cool, feed, pet) {
+	  this.cool = cool
+	  this.feed = function (dog) {//dog parameter is pickles as long as it passes???
+	    return dog.hungry = false
+	  }
+	  this.pet = function (dog) {
+	    return dog.status = 'happy'
+	  }
+	}
 
 
-// Instances of Human
-// Needed: mason, julia
+	// Instances of Human
+	// Needed: mason, julia
 
-const mason = new Human('mason', false);
-const julia = new Human('julia', true);
+
+	const julia = new Human(true)
+	const mason = new Human(false)
